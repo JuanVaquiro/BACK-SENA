0ce2dab6524c1365f189e58a0ee40a2cfccb0100
unknown <jvaquiro23@itfip.edu.co>
2022-10-21 15:09:25 -0500

    CRUD CLIENT

--- function.php
+++ function.php
@@ -9,29 +9,24 @@ function GetProductID($id)
     $sentencia->execute([$id]);
     return $sentencia->fetchObject();
 }
-
 function GetProduct()
 {
     $bd = obtenerConexion();
     $sentencia = $bd->query(sqlSelectPrdocut());
     return $sentencia->fetchAll();
 }
-
 function InsertProduct($product)
 {
     $bd = obtenerConexion();
     $sentencia = $bd->prepare(sqlInsertPrdocut());
-    return $sentencia->execute([ $product->nombre_producto, $product->marca, $product->modelo, $product->precio, $product->stock ]);
+    return $sentencia->execute([$product->nombre_producto, $product->marca, $product->modelo, $product->precio, $product->stock]);
 }
-
 function UpdateProduct($product)
 {
     $bd = obtenerConexion();
     $sentencia = $bd->prepare(sqlUpdatePrdocut());
     return $sentencia->execute([$product->nombre_producto, $product->marca, $product->modelo, $product->precio, $product->stock, $product->id]);
 }
-
-
 function DeletProduct($id)
 {
     $bd = obtenerConexion();
@@ -41,9 +36,34 @@ function DeletProduct($id)
 
 
 
+function GetClienttID($id)
+{
+    $bd = obtenerConexion();
+    $sentencia = $bd->prepare(sqlSelectClientID());
+    $sentencia->execute([$id]);
+    return $sentencia->fetchObject();
+}
 function GetClient()
 {
     $bd = obtenerConexion();
     $sentencia = $bd->query(sqlSelectClient());
     return $sentencia->fetchAll();
+}
+function InserClient($client)
+{
+    $bd = obtenerConexion();
+    $sentencia = $bd->prepare(sqlInsertClient());
+    return $sentencia->execute([ $client->nombre, $client->apellido, $client->direccion, $client->telefono, $client->email ]);
+}
+function UpdateClient($client)
+{
+    $bd = obtenerConexion();
+    $sentencia = $bd->prepare(sqlUpdateClient());
+    return $sentencia->execute([$client->nombre, $client->apellido, $client->direccion, $client->telefono, $client->email, $client->id]);
+}
+function DeletClient($id)
+{
+    $bd = obtenerConexion();
+    $sentencia = $bd->prepare(sqlDeleteClient());
+    return $sentencia->execute([$id]);
 }
--- querys.php
+++ querys.php
@@ -1,29 +1,27 @@
 <?php
 
-function sqlSelectIDPrdocut(){
-    $queryProductID = "SELECT * FROM productos WHERE id = ?";
-    return $queryProductID;
-}
-
 function sqlSelectPrdocut(){
     $queryProduct = "SELECT * FROM productos WHERE 1";
     return $queryProduct;
 }
-
+function sqlSelectIDPrdocut(){
+    $queryProductID = "SELECT * FROM productos WHERE id = ?";
+    return $queryProductID;
+}
 function sqlInsertPrdocut(){
     $queryProduct = "INSERT INTO productos(nombre_producto, marca, modelo, precio, stock) VALUES (?, ?, ?, ?, ?)";
     return $queryProduct;
 }
-
+function sqlUpdatePrdocut(){
+    $queryUpdateProduct = "UPDATE productos SET nombre_producto = ?, marca = ?, modelo = ?, precio = ?, stock = ?
+     WHERE id = ?";
+    return $queryUpdateProduct;
+}
 function sqlDeletePrdocut(){
     $queryDeletProduct = "DELETE FROM productos WHERE id = ?";
     return $queryDeletProduct;
 }
 
-function sqlUpdatePrdocut(){
-    $queryUpdateProduct = "UPDATE productos SET nombre_producto = ?, marca = ?, modelo = ?, precio = ?, stock = ? WHERE id = ?";
-    return $queryUpdateProduct;
-}
 
 
 function sqlSelectClient(){
@@ -31,4 +29,21 @@ function sqlSelectClient(){
     return $queryClient;
 }
 
- 
+function sqlSelectClientID(){
+    $queryClientID = "SELECT * FROM clientes WHERE id = ?";
+    return $queryClientID;
+}
+
+function sqlInsertClient(){
+    $InsertClient = "INSERT INTO clientes(nombre, apellido, direccion, telefono, email) VALUES (?, ?, ?, ?, ?)";
+    return $InsertClient;
+}
+function sqlUpdateClient(){
+    $queryUpdateClient = "UPDATE clientes SET nombre = ?, apellido  = ?, direccion  = ?, telefono  = ?, email = ? 
+    WHERE id = ?";
+    return $queryUpdateClient;
+}
+function sqlDeleteClient(){
+    $queryDeletClient = "DELETE FROM clientes WHERE id = ?";
+    return $queryDeletClient;
+}
